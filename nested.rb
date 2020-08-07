cdf8a63e51b0363b342d66d0c00625b202980d09
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-07 01:43:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
-  programmer_hash[:yukihiro_matsumoto][:languages] << "C"
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
   
   programmer_hash  
 end
